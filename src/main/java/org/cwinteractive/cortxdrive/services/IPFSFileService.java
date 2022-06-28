7397a17db549e31d34b6c44b441206660c5cadfd
Kuro Souza <=>
2022-06-29 00:46:55 +0100

    Docker and docker-compose setup.

--- src/main/java/org/cwinteractive/cortxdrive/services/IPFSFileService.java
+++ src/main/java/org/cwinteractive/cortxdrive/services/IPFSFileService.java
@@ -2,7 +2,6 @@ package org.cwinteractive.cortxdrive.services;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.InputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -25,11 +24,6 @@ import org.springframework.web.client.RestTemplate;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import io.ipfs.api.IPFS;
-import io.ipfs.api.MerkleNode;
-import io.ipfs.api.NamedStreamable;
-import io.ipfs.multihash.Multihash;
-
 @Service
 public class IPFSFileService {
 	
@@ -38,22 +32,15 @@ public class IPFSFileService {
 
 	Logger logger = LoggerFactory.getLogger(IPFSFileService.class);
 	
-	private IPFS ipfs;
-	
 	@Value("${ipfs.addFileUrl}")
 	String ipfsAddFileUrl;
 	
 	@Value("${ipfs.retrieveFileUrl}")
 	String ipfsRetrieveFileUrl;
 	
-	public IPFSFileService(IPFS ipfs) {
-		this.ipfs = ipfs;
-	}
+	public IPFSFileService() { }
 	
 	public Map<String,Object> save(File file) throws Exception {
-		// NamedStreamable.FileWrapper fileWrapper = new NamedStreamable.FileWrapper(file);
-		// MerkleNode addResult = ipfs.add(fileWrapper).get(0);		
-		// return addResult.toJSONString();
 		
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.MULTIPART_FORM_DATA);
@@ -73,19 +60,6 @@ public class IPFSFileService {
 		
 		return responseMap;
 	
-	}
-	
-	public String save(String fileName, InputStream inputStream) throws Exception {
-		byte[] fileData = inputStream.readAllBytes(); 
-		NamedStreamable.ByteArrayWrapper fileDataWrapper = new NamedStreamable.ByteArrayWrapper(fileName, fileData);		
-		MerkleNode addResult = ipfs.add(fileDataWrapper).get(0);		
-		return addResult.toJSONString();
-	}
-	
-	public byte[] getFileContents(String cid) throws Exception {
-		Multihash filePointer = Multihash.fromBase58(cid);
-		byte[] fileContents = ipfs.cat(filePointer);
-		return fileContents;
 	}
 	
 	public File download(String cid, String fileName) throws Exception {
